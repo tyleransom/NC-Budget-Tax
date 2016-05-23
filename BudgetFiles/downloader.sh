c8e1a0cf3bed9941fca22ed1e18f1fae2614f92f
Tyler Ransom <tyleransom@gmail.com>
2016-05-23 12:10:13 -0400

    Initial commit

---
+++ BudgetFiles/downloader.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Download budget PDFs
+wget -O 2003_05/vol1.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/1200305_BD307_Vol1.pdf
+wget -O 2003_05/vol2.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2200305_BD307_Vol2.pdf
+wget -O 2003_05/vol3.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/3200305_BD307_Vol3.pdf
+wget -O 2003_05/vol4.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/4200305_BD307_Vol4.pdf
+wget -O 2003_05/vol5.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/5200305_BD307_Vol5.pdf
+wget -O 2003_05/vol6.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/6200305_BD307_Vol6.pdf
+
