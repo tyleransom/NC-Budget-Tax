e76087958c1ebcfc2bb53aa17e73a85525231d2a
astr93 <artem.streltsov@duke.edu>
2016-05-24 09:59:13 -0400

    downloader in Windows batch syntax
    currently for 2003_5 only

---
+++ BudgetFiles/downloader.bat
@@ -0,0 +1,7 @@
+md "2003_5"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/1200305_BD307_Vol1.pdf -O "2003_5\vol1.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2200305_BD307_Vol2.pdf -O "2003_5\vol2.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/3200305_BD307_Vol3.pdf -O "2003_5\vol3.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/4200305_BD307_Vol4.pdf -O "2003_5\vol4.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/5200305_BD307_Vol5.pdf -O "2003_5\vol5.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/6200305_BD307_Vol6.pdf -O "2003_5\vol6.pdf"
