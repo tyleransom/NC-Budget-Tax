b2e5675393623e0d64769753dc94c6839ace63fb
astr93 <artem.streltsov@duke.edu>
2016-06-01 12:47:42 -0400

    "final" downloader
    As usual, make sure the directories are specified correctly for you (for wget and data+ storage folders).

--- BudgetFiles/downloader.bat
+++ BudgetFiles/downloader.bat
@@ -1,3 +1,4 @@
+cd/ & cd C:/Users/Tom/Desktop/Data+
 md "2003_5"
 "C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/1200305_BD307_Vol1.pdf -O "2003_5\vol1.pdf"
 "C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2200305_BD307_Vol2.pdf -O "2003_5\vol2.pdf"
@@ -5,3 +6,94 @@ md "2003_5"
 "C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/4200305_BD307_Vol4.pdf -O "2003_5\vol4.pdf"
 "C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/5200305_BD307_Vol5.pdf -O "2003_5\vol5.pdf"
 "C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/6200305_BD307_Vol6.pdf -O "2003_5\vol6.pdf"
+
+cd/ & cd C:/Users/Tom/Desktop/Data+
+md "2004_5"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/1200405_BD307_Vol1.pdf -O "2004_5\vol1.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2200405_BD307_Vol2.pdf -O "2004_5\vol2.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/3200405_BD307_Vol3.pdf -O "2004_5\vol3.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/4200405_BD307_Vol4.pdf -O "2004_5\vol4.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/5200405_BD307_Vol5.pdf -O "2004_5\vol5.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/6200405_BD307_Vol6.pdf -O "2004_5\vol6.pdf"
+
+cd/ & cd C:/Users/Tom/Desktop/Data+
+md "2005_7"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/1a200507_BD307_Vol1.pdf -O "2005_7\vol1.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2a200507_BD307_Vol2.pdf -O "2005_7\vol2.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/3a200507_BD307_Vol3.pdf -O "2005_7\vol3.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/4a200507_BD307_Vol4.pdf -O "2005_7\vol4.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/5a200507_BD307_Vol5.pdf -O "2005_7\vol5.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/6a200507_BD307_Vol6.pdf -O "2005_7\vol6.pdf"
+
+cd/ & cd C:/Users/Tom/Desktop/Data+
+md "2006_7"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/1b200607_BD307_Vol1.pdf -O "2006_7\vol1.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2b200607_BD307_Vol2.pdf -O "2006_7\vol2.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/3b200607_BD307_Vol3.pdf -O "2006_7\vol3.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/4b200607_BD307_Vol4.pdf -O "2006_7\vol4.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/5b200607_BD307_Vol5.pdf -O "2006_7\vol5.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/6b200607_BD307_Vol6.pdf -O "2006_7\vol6.pdf"
+
+cd/ & cd C:/Users/Tom/Desktop/Data+
+md "2007_9"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/200709_BD307_Vol1.pdf -O "2007_9\vol1.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/200709_BD307_Vol2.pdf -O "2007_9\vol2.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/200709_BD307_Vol3.pdf -O "2007_9\vol3.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/200709_BD307_Vol4.pdf -O "2007_9\vol4.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/200709_BD307_Vol5.pdf -O "2007_9\vol5.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/200709_BD307_Vol6.pdf -O "2007_9\vol6.pdf"
+
+cd/ & cd C:/Users/Tom/Desktop/Data+
+md "2008_9"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/200809-BD307-Vol1.pdf -O "2008_9\vol1.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/200809-BD307-Vol2.pdf -O "2008_9\vol2.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/200809-BD307-Vol3.pdf -O "2008_9\vol3.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/200809-BD307-Vol4.pdf -O "2008_9\vol4.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/200809-BD307-Vol5.pdf -O "2008_9\vol5.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/200809-BD307-Vol6.pdf -O "2008_9\vol6.pdf"
+
+cd/ & cd C:/Users/Tom/Desktop/Data+
+md "2009_11"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/1aBD307_2009-11_Vol1.pdf -O "2009_11\vol1.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2aBD307_2009-11_Vol2.pdf -O "2009_11\vol2.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/3aBD307_2009-11_Vol3.pdf -O "2009_11\vol3.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/4aBD307_2009-11_Vol4.pdf -O "2009_11\vol4.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/5aBD307_2009-11_Vol5.pdf -O "2009_11\vol5.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/6aBD307_2009-11_Vol6.pdf -O "2009_11\vol6.pdf"
+
+cd/ & cd C:/Users/Tom/Desktop/Data+
+md "2010_11"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2010-BD307-V1.pdf -O "2010_11\vol1.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2010-BD307-V2.pdf -O "2010_11\vol2.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2010-BD307-V3.pdf -O "2010_11\vol3.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2010-BD307-V4.pdf -O "2010_11\vol4.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2010-BD307-V5.pdf -O "2010_11\vol5.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2010-BD307-V6.pdf -O "2010_11\vol6.pdf"
+
+cd/ & cd C:/Users/Tom/Desktop/Data+
+md "2011_13"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2011-BD307-V1.pdf -O "2011_13\vol1.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2011-BD307-V2.pdf -O "2011_13\vol2.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2011-BD307-V3.pdf -O "2011_13\vol3.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2011-BD307-V4.pdf -O "2011_13\vol4.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2011-BD307-V5.pdf -O "2011_13\vol5.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2011-BD307-V6.pdf -O "2011_13\vol6.pdf"
+
+cd/ & cd C:/Users/Tom/Desktop/Data+
+md "2012_13"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2012_BD307_Vol1.pdf -O "2012_13\vol1.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2012_BD307_Vol2.pdf -O "2012_13\vol2.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2012_BD307_Vol3.pdf -O "2012_13\vol3.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2012_BD307_Vol4.pdf -O "2012_13\vol4.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2012_BD307_Vol5.pdf -O "2012_13\vol5.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/2012_BD307_Vol6.pdf -O "2012_13\vol6.pdf"
+
+cd/ & cd C:/Users/Tom/Desktop/Data+
+md "2014_15"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/Certified_2014-15_Education.pdf -O "2014_15\vol1.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/Certified_2014-15_GenGov.pdf -O "2014_15\vol2.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/Certified_2014-15_DHHS.pdf -O "2014_15\vol3.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/Certified_2014-15_JPS.pdf -O "2014_15\vol4.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/Certified_2014-15_NER.pdf -O "2014_15\vol5.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/Certified_2014-15_Transportation.pdf -O "2014_15\vol6.pdf"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe" http://ncosbm.s3.amazonaws.com/s3fs-public/Certified_2014-15_Statewide.pdf -O "2014_15\vol6.pdf"
