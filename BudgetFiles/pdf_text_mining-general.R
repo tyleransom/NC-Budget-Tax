baa1d2c70b61fd2e55d8555aa0bca8fbcdcf3263
Tyler Ransom <tyleransom@gmail.com>
2016-06-02 10:45:20 -0400

    Cleaned up PDF cleaning file

--- BudgetFiles/pdf_text_mining-general.R
+++ BudgetFiles/pdf_text_mining-general.R
@@ -15,40 +15,17 @@ require(tabulizer)
 
 # tell it where the PDF is
 file <- "C:/Users/admin/Desktop/DUKE Data+/2003_5/vol6.pdf"
+
 a <- get_n_pages(file=file)
 # extract tables from the PDF
-genout1 <- extract_tables(file, pages = 1, guess = FALSE, method = "data.frame")
-#a <- 
-#library(xlsx)
-Genout1 <- as.data.frame(genout1)
-#write.xlsx(out1, file="filename.xlsx", sheetName="sheet1")
-
-for (i in 2:a)
-{try({out <- extract_tables(file, pages = i, guess = FALSE, method = "data.frame")
-
-# make into data frame 
-Out <- as.data.frame(out)
-#nextsheet <- paste("sheet", i, sep = "")
-Genout1 <- rbind(Genout1,Out)
-#write.xlsx(out, file="filename.xlsx", sheetName=paste("sheet", i, sep = ""), append=TRUE)
+Genout1 <- as.data.frame(extract_tables(file, pages = 1, guess = FALSE, method = "data.frame", columns = list(c(320, 440))))
+for (i in 2:a) {
+	try({
+	Out <- as.data.frame(extract_tables(file, pages = i, guess = FALSE, method = "data.frame", columns = list(c(320, 440))))
+	Genout1 <- rbind(Genout1,Out)
+	})
 }
-)
-}
-#print(out1)
-#write.xlsx(out1, file="File1.xlsx")
+
 firstletter <- as.data.frame(substr(Genout1[,1],1,1))
-#write.xlsx(out, file="filename.xlsx", sheetIndex=i, append=TRUE)}
-#x <- c()
-
-#for ( b in 1:4741 )
-  #if (toString(firstletter[b,], width = NULL) == "-" ){
- #       append( x, b )
-#    }
-Genout1 = as.data.frame(Genout[!(firstletter == "-"), ])
-
-
-
-
-
-
+Genout1 <- as.data.frame(Genout1[!(firstletter == "-"), ])
 
