df6372b39f5115332db11f4fed6b11a7e7ddd752
Tyler Ransom <tyleransom@gmail.com>
2016-06-03 13:04:12 -0400

    Tyler's updates to main file

--- BudgetFiles/pdf_text_mining-general.R
+++ BudgetFiles/pdf_text_mining-general.R
@@ -21,44 +21,101 @@ file <- "C:/Users/admin/Desktop/DUKE Data+/2003_5/vol6.pdf"
 
 a <- get_n_pages(file=file)
 # extract tables from the PDF
-Genout1 <- as.data.frame(extract_tables(file, pages = 1, guess = FALSE, method = "data.frame", columns = list(c(320, 440))))
+Genout1 <- as.data.frame(extract_tables(file, pages = 1, guess = FALSE, method = "data.frame", columns = list(c(320, 420))),stringsAsFactors=FALSE)
 for (i in 2:a) {
 	try({
-	Out <- as.data.frame(extract_tables(file, pages = i, guess = FALSE, method = "data.frame", columns = list(c(320, 440))))
+	Out <- as.data.frame(extract_tables(file, pages = i, guess = FALSE, method = "data.frame", columns = list(c(320, 420))),stringsAsFactors=FALSE)
 	Genout1 <- rbind(Genout1,Out)
 	})
 }
 
 
-Genout1 <- as.data.frame(Genout1[!(as.data.frame(substr(Genout1[,1],1,1)) == "-"), ])
+Genout1 <- as.data.frame(Genout1[!(as.data.frame(substr(Genout1[,1],1,1)) == "-"), ],stringsAsFactors=FALSE)
 
-Genout2=Genout1
-Gen2 <- as.data.frame(gsub("TOTAL", "00000 TOTAL", Genout2[,1]))
+# Fix columns that begin with strings that need to begin with numbers
+Genout2 <- Genout1
+Gen2 <- as.data.frame(gsub("TOTAL", "00000 TOTAL", Genout2[,1]),stringsAsFactors=FALSE)
 Gen3 <- cbind(Gen2, Genout2[,2:3])
 
-Gen2 <- as.data.frame(gsub("NET", "00001 NET", Gen3[,1]))
+Gen2 <- as.data.frame(gsub("NET", "00001 NET", Gen3[,1]),stringsAsFactors=FALSE)
 Gen3 <- cbind(Gen2, Gen3[,2:3])
 
-Gen2 <- as.data.frame(gsub("HIGHWAY", "00002 HIGHWAY", Gen3[,1]))
+Gen2 <- as.data.frame(gsub("HIGHWAY", "00002 HIGHWAY", Gen3[,1]),stringsAsFactors=FALSE)
 Gen3 <- cbind(Gen2, Gen3[,2:3])
 
-Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,2)) == "42"), ])
-Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,3)) == "APP"), ])
-Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,3)) == "BUD"), ])
-Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,3)) == "REQ"), ])
-Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,3)) == "142"), ])
-Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,3)) == "DES"), ])
-Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,3)) == "EST"), ])
-Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,3)) == "POS"), ])
-Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,3)) == "SUM"), ])
+Gen2 <- as.data.frame(gsub("CHANG", "00003 CHANG", Gen3[,1]),stringsAsFactors=FALSE)
+Gen3 <- cbind(Gen2, Gen3[,2:3])
 
-Gen4 <- as.data.frame(gsub(",","",Gen3[,2]))
-Gen42 <- as.data.frame(gsub(",","",Gen3[,3]))
+# Remove unnecessary lines
+Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,2)) == "42" ), ],stringsAsFactors=FALSE)
+Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,3)) == "APP"), ],stringsAsFactors=FALSE)
+Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,3)) == "BUD"), ],stringsAsFactors=FALSE)
+Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,3)) == "REQ"), ],stringsAsFactors=FALSE)
+Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,3)) == "142"), ],stringsAsFactors=FALSE)
+Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,3)) == "DES"), ],stringsAsFactors=FALSE)
+Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,3)) == "EST"), ],stringsAsFactors=FALSE)
+Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,3)) == "POS"), ],stringsAsFactors=FALSE)
+Gen3 <- as.data.frame(Gen3[!(as.data.frame(substr(Gen3[,1],1,3)) == "SUM"), ],stringsAsFactors=FALSE)
+
+# Remove commas from dollar amounts and any lines with "PAGE" in the final column
+Gen4 <- as.data.frame(gsub(",","",Gen3[,2]),stringsAsFactors=FALSE)
+Gen42 <- as.data.frame(gsub(",","",Gen3[,3]),stringsAsFactors=FALSE)
 Gen4 <- cbind(Gen3[,1], Gen4, Gen42)
 Gen4 <- Gen4[!apply(Gen4 == "", 1, all),]
+Gen4 <- Gen4[!grepl("PAGE",Gen4[,3]),]
 Gen4 <- Gen4
 
-numbers <- as.data.frame(substr(Gen4[,1],1,5))
-letters <- as.data.frame(substr(Gen4[,1],6,100))
+# Start concatenating the vector
+numbers <- as.data.frame(substr(Gen4[,1],1,5),stringsAsFactors=FALSE)
+letters <- as.data.frame(substr(Gen4[,1],6,100),stringsAsFactors=FALSE)
 
+# Aggregate together, rename columns, and convert IDs to numeric
 Genfin <- cbind(numbers, letters, Gen4[,2:3])
+colnames(Genfin) <- c("SubsecID","Description","y200304","y200405")
+Genfin$SubsecID  <- as.numeric(Genfin$SubsecID)
+Genfin$y200304   <-type.convert(Genfin$y200304, numerals="warn.loss");
+Genfin$y200405   <-type.convert(Genfin$y200405, numerals="warn.loss");
+
+# List all categories with given ID
+Genfin[Genfin$SubsecID==6990,]
+
+
+### LEFT TO DO:
+# Somehow map the "big" and "small" codes where there are currently NA's
+# into two new columns labeled "Category" and "Subcategory"
+
+# I found this function at http://www.r-bloggers.com/generating-a-laglead-variables/
+shift<-function(x,shift_by){
+    stopifnot(is.numeric(shift_by))
+    stopifnot(is.numeric(x))
+ 
+    if (length(shift_by)>1)
+        return(sapply(shift_by,shift, x=x))
+ 
+    out<-NULL
+    abs_shift_by=abs(shift_by)
+    if (shift_by > 0 )
+        out<-c(tail(x,-abs_shift_by),rep(NA,abs_shift_by))
+    else if (shift_by < 0 )
+        out<-c(rep(NA,abs_shift_by), head(x,-abs_shift_by))
+    else
+        out<-x
+    out
+}
+
+
+# Flag aggregate codes and sub-aggregate codes so that we can get a unique identifier
+Genfin$aggflag    <- is.na(Genfin$y200304)
+Genfin$subaggflag <- is.na(Genfin$y200304)
+Genfin$IDflag     <- as.logical(shift(as.numeric((!is.na(Genfin$y200304) & Genfin$lead1==TRUE)),-1))
+Genfin$subIDflag  <- as.logical(shift(as.numeric((!is.na(Genfin$y200304) & Genfin$lag1==TRUE)),1))
+Genfin$Category   <- as.numeric(Genfin$IDflag)*Genfin$SubsecID
+Genfin$Subategory <- as.numeric(Genfin$subIDflag)*Genfin$SubsecID
+Genfin$Category[1]<- Genfin$SubsecID[1]
+
+# Now we need to fill in the zeros between the numbers with the previously found number.
+# This will probably require looping through values
+# i.e.
+# look at next observation
+# if next observation is 0, replace it with current observation
+# otherwise continue to the next observation and repeat
