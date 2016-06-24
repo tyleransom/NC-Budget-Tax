c84c743def283853371a74284d934ab0c237341e
Tyler Ransom <tyleransom@gmail.com>
2016-06-24 15:57:01 -0400

    Added PDFs to repo

--- BudgetFiles/downloader.sh
+++ BudgetFiles/downloader.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Download budget PDFs
+mkdir 2003_5
 wget -O 2003_5/vol1.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/1200305_BD307_Vol1.pdf
 wget -O 2003_5/vol2.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2200305_BD307_Vol2.pdf
 wget -O 2003_5/vol3.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/3200305_BD307_Vol3.pdf
@@ -8,6 +9,7 @@ wget -O 2003_5/vol4.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/4200305_BD307
 wget -O 2003_5/vol5.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/5200305_BD307_Vol5.pdf
 wget -O 2003_5/vol6.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/6200305_BD307_Vol6.pdf
 
+mkdir 2004_5
 wget -O 2004_5/vol1.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/1200405_BD307_Vol1.pdf
 wget -O 2004_5/vol2.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2200405_BD307_Vol2.pdf
 wget -O 2004_5/vol3.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/3200405_BD307_Vol3.pdf
@@ -15,6 +17,7 @@ wget -O 2004_5/vol4.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/4200405_BD307
 wget -O 2004_5/vol5.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/5200405_BD307_Vol5.pdf
 wget -O 2004_5/vol6.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/6200405_BD307_Vol6.pdf
 
+mkdir 2005_7
 wget -O 2005_7/vol1.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/1a200507_BD307_Vol1.pdf
 wget -O 2005_7/vol2.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2a200507_BD307_Vol2.pdf
 wget -O 2005_7/vol3.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/3a200507_BD307_Vol3.pdf
@@ -22,6 +25,7 @@ wget -O 2005_7/vol4.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/4a200507_BD30
 wget -O 2005_7/vol5.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/5a200507_BD307_Vol5.pdf
 wget -O 2005_7/vol6.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/6a200507_BD307_Vol6.pdf
 
+mkdir 2006_7
 wget -O 2006_7/vol1.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/1b200607_BD307_Vol1.pdf
 wget -O 2006_7/vol2.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2b200607_BD307_Vol2.pdf
 wget -O 2006_7/vol3.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/3b200607_BD307_Vol3.pdf
@@ -29,6 +33,7 @@ wget -O 2006_7/vol4.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/4b200607_BD30
 wget -O 2006_7/vol5.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/5b200607_BD307_Vol5.pdf
 wget -O 2006_7/vol6.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/6b200607_BD307_Vol6.pdf
 
+mkdir 2007_9
 wget -O 2007_9/vol1.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/200709_BD307_Vol1.pdf
 wget -O 2007_9/vol2.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/200709_BD307_Vol2.pdf
 wget -O 2007_9/vol3.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/200709_BD307_Vol3.pdf
@@ -36,6 +41,7 @@ wget -O 2007_9/vol4.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/200709_BD307_
 wget -O 2007_9/vol5.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/200709_BD307_Vol5.pdf
 wget -O 2007_9/vol6.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/200709_BD307_Vol6.pdf
 
+mkdir 2008_9
 wget -O 2008_9/vol1.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/200809-BD307-Vol1.pdf
 wget -O 2008_9/vol2.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/200809-BD307-Vol2.pdf
 wget -O 2008_9/vol3.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/200809-BD307-Vol3.pdf
@@ -43,6 +49,7 @@ wget -O 2008_9/vol4.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/200809-BD307-
 wget -O 2008_9/vol5.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/200809-BD307-Vol5.pdf
 wget -O 2008_9/vol6.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/200809-BD307-Vol6.pdf
 
+mkdir 2009_11
 wget -O 2009_11/vol1.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/1aBD307_2009-11_Vol1.pdf
 wget -O 2009_11/vol2.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2aBD307_2009-11_Vol2.pdf
 wget -O 2009_11/vol3.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/3aBD307_2009-11_Vol3.pdf
@@ -50,6 +57,7 @@ wget -O 2009_11/vol4.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/4aBD307_2009
 wget -O 2009_11/vol5.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/BD307_2009-11_Vol5.pdf
 wget -O 2009_11/vol6.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/BD307_2009-11_Vol6.pdf
 
+mkdir 2010_11
 wget -O 2010_11/vol1.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2010-BD307-V1.pdf
 wget -O 2010_11/vol2.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2010-BD307-V2.pdf
 wget -O 2010_11/vol3.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2010-BD307-V3.pdf
@@ -57,6 +65,7 @@ wget -O 2010_11/vol4.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2010-BD307-V
 wget -O 2010_11/vol5.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2010-BD307-V5.pdf
 wget -O 2010_11/vol6.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2010-BD307-V6.pdf
 
+mkdir 2011_13
 wget -O 2011_13/vol1.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2011-BD307-V1.pdf
 wget -O 2011_13/vol2.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2011-BD307-V2.pdf
 wget -O 2011_13/vol3.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2011-BD307-V3.pdf
@@ -64,6 +73,7 @@ wget -O 2011_13/vol4.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2011-BD307-V
 wget -O 2011_13/vol5.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2011-BD307-V5.pdf
 wget -O 2011_13/vol6.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2011-BD307-V6.pdf
 
+mkdir 2012_13
 wget -O 2012_13/vol1.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2012_BD307_Vol1.pdf
 wget -O 2012_13/vol2.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2012_BD307_Vol2.pdf
 wget -O 2012_13/vol3.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2012_BD307_Vol3.pdf
@@ -71,6 +81,7 @@ wget -O 2012_13/vol4.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2012_BD307_V
 wget -O 2012_13/vol5.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2012_BD307_Vol5.pdf
 wget -O 2012_13/vol6.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/2012_BD307_Vol6.pdf
 
+mkdir 2014_15
 wget -O 2014_15/vol1.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/Certified_2014-15_Education.pdf
 wget -O 2014_15/vol2.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/Certified_2014-15_GenGov.pdf
 wget -O 2014_15/vol3.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/Certified_2014-15_DHHS.pdf
@@ -79,6 +90,7 @@ wget -O 2014_15/vol5.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/Certified_20
 wget -O 2014_15/vol6.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/Certified_2014-15_Transportation.pdf
 wget -O 2014_15/vol6.pdf http://ncosbm.s3.amazonaws.com/s3fs-public/Certified_2014-15_Statewide.pdf
 
+mkdir 2013_15
 wget -O 2013_15/PublicEducation.pdf                                             http://ncosbm.s3.amazonaws.com/s3fs-public/BD307_2013-15_080_DPI.pdf
 wget -O 2013_15/NCCommunityCollegeSystem.pdf                                    http://ncosbm.s3.amazonaws.com/s3fs-public/BD307_2013-15_500_NCCCS.pdf
 wget -O 2013_15/UniversityOfNorthCarolinaGeneralAdministration.pdf              http://ncosbm.s3.amazonaws.com/s3fs-public/BD307_2013-15_U10_UNCGA.pdf
@@ -145,6 +157,7 @@ wget -O 2013_15/NCEducationLottery.pdf                                          
 wget -O 2013_15/Transportation.pdf                                              http://ncosbm.s3.amazonaws.com/s3fs-public/BD307_2013-15_150_Transportation.pdf
 wget -O 2013_15/StatewideReservesDebtServiceandOtherAdjustments.pdf             http://ncosbm.s3.amazonaws.com/s3fs-public/BD307_2013-15_AAA_Statewide.pdf
 
+mkdir 2015_17
 wget -O 2015_17/PublicEducation.pdf                                             https://ncosbm.s3.amazonaws.com/s3fs-public/documents/files/2015-17_Certified_080_DPI.pdf
 wget -O 2015_17/NCCommunityCollegeSystem.pdf                                    https://ncosbm.s3.amazonaws.com/s3fs-public/documents/files/2015-17_Certified_500_NCCCS.pdf
 wget -O 2015_17/UniversityOfNorthCarolinaGeneralAdministration.pdf              https://ncosbm.s3.amazonaws.com/s3fs-public/documents/files/2015-17_Certified_U10_UNCGA.pdf
